597cbee0565d34a29efd69ededf971312d30b0e7
Жеребко Дмитрий < >
2016-06-17 13:02:44 +0300

    still fixing

--- task10.py
+++ task10.py
@@ -1,5 +1,7 @@
 class MySet(object):
-    myset_struct=[]
+    
+    def __init__(self):
+          self.myset_struct=[]
     
     def add(self,val):
         if(not self.myset_struct.__contains__(val)):
@@ -9,38 +11,48 @@ class MySet(object):
         self.myset_struct.remove(val)
     
     def union(self ,values):
-        newList=self;
         for v in values.myset_struct:
-            newList.add(v)
-        return newList    
+            self.add(v)
+        self.myset_struct.sort()
+        return self.myset_struct  
 
     def intersection(self,values):
-        newList=[]
         for y in values.myset_struct:
             if(self.myset_struct.__contains__(y)):
-                newList.append(y)
-        return newList
+                self.add(y)
+        self.myset_struct.sort()  
+        print(self.myset_struct)        
+        return self.myset_struct
     
     def diff(self,values):
         newList=[]
         for y in values.myset_struct:
             if(not self.myset_struct.__contains__(y)):
                 newList.append(y)
+        newList.sort()
+        self.myset_struct=newList;         
         return newList
     def __str__(self):
         return  str(self.myset_struct)
     
-
+  
 mySet=MySet()
-for a in range(0,100):
+for a in range(1,15,3):
     mySet.add(a)
     
 
+print(" set1 ",mySet)
 l=MySet()
-for a in range(100,200):
+for a in range(3,10):
     l.add(a)
 
-print(mySet.union(l))
-print(mySet.diff(mySet))
+print("set2 ",l)
 
+
+
+
+print("set1 and set 2 union",mySet.union(l))
+print(mySet.myset_struct)
+print("set1 and set 2 diff",mySet.diff(l))
+print("set1 and set 2 intersection",mySet.intersection(l))
   
